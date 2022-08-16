c03f40dd037d7b699068787e72a26153cef2a316
zingw <vietduc.bk69@gmail.com>
2022-08-16 23:16:48 +0700

    inits tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,75 +8,11 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   arr1 = ['1','2','3','/', '4','5','6','*', '7','8','9','-', '0','.','=','+'];
-  input = '';
-  result = 0;
-  INVALID_ERR = "INVALID_ERR"
-  pressNum(number: string) {
-    if(this.isValidToAdd(number)){
-      this.input += number;
-    }
-    else alert(this.INVALID_ERR)
-  }
-
-  getResult(): number {
-    this.result = 1231245121;
-    return this.result;
-  }
-
-  isValidToAdd(x: string): boolean {
-    // nếu input ban đầu rỗng thì cứ return true
-    if(this.input==''){
-      return true;
-    }
-
-    // Nếu lastChar là OP thì sau nó phải là 1 number
-    if(this.isOperator(this.getLastInputChar()) && this.isNumber(x)){
-      return true;
-    }
-    // Nếu lastChar là number thì sau nó có thể là 1 number hoặc OP
-    if(this.isNumber(this.getLastInputChar())){
-      return true;
-    }
-    else{
-      alert(this.INVALID_ERR);
-    }
-    return false;
-  }
-
-  isValidToCalculate():boolean{
-    if(this.isValidToAdd(this.input) && this.isNumber(this.getLastInputChar())){
-      return true;
-    }
-    else{
-      alert(this.INVALID_ERR)
-    }
-    return false;
-  }
-
-  isOperator(x: string): boolean {
-    let arr = ['+', '-', '*', '/'];
-    return arr.lastIndexOf(x) > -1 ? true : false;
-  }
-
-  isNumber(x: string): boolean {
-    return !isNaN(Number(x));
-  }
-
-  getLastInputChar(): string {
-    if(this.getLastInputChar() ==''){
-      this.input = '0';
-      return '0';
-    }
-    return this.input[this.input.length - 1];
-  }
-
-  remove() {
-    this.input = this.input.substring(0, this.input.length - 1);
-  }
-
-  getValue(item: string) {
-    console.log(item);
-
-  }
+  tables : Tables[] = [
+    {col1:'7',col2:'8',col3:'9',col4:'/'},
+    {col1:'4',col2:'5',col3:'6',col4:'x'},
+    {col1:'1',col2:'2',col3:'3',col4:'-'},
+    {col1:'0',col2:'.',col3:'C',col4:'+'},
+  ];
 }
 
