9a5052b263512e52f4d7085dabbea22b4edad526
zingw <vietduc.bk69@gmail.com>
2022-08-17 00:32:08 +0700

    init table sucess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
 import {Component} from '@angular/core';
 
+
+export interface Row {
+  col1: string ;
+  col2: string ;
+  col3: string ;
+  col4: string ;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
-  arr1 = ['1','2','3','/', '4','5','6','*', '7','8','9','-', '0','.','=','+'];
-  tables : Tables[] = [
+export class AppComponent {
+input = '';
+result = 0;
+INVALID_ERR = "INVALID_ERR";
+
+rows : Row[] = [
     {col1:'7',col2:'8',col3:'9',col4:'/'},
     {col1:'4',col2:'5',col3:'6',col4:'x'},
     {col1:'1',col2:'2',col3:'3',col4:'-'},
     {col1:'0',col2:'.',col3:'C',col4:'+'},
   ];
+
 }
+
+
+
 
