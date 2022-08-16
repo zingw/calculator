4f646c3ff57ebd4cf2fab053625a471e2ec04b5b
zingw <vietduc.bk69@gmail.com>
2022-08-16 17:32:07 +0700

    ve nha lam tiep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +6,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  input = '';
 
-  getAlert() {
-    alert("haha")
+  arr1 = ['1','2','3','/', '4','5','6','*', '7','8','9','-', '0','.','=','+'];
+  input = '';
+  result = 0;
+  INVALID_ERR = "INVALID_ERR"
+  pressNum(number: string) {
+    if(this.isValidToAdd(number)){
+      this.input += number;
+    }
+    else alert(this.INVALID_ERR)
   }
 
-  pressNum(s: string) {
-    this.input+= s;
+  getResult(): number {
+    this.result = 1231245121;
+    return this.result;
+  }
+
+  isValidToAdd(x: string): boolean {
+    // nếu input ban đầu rỗng thì cứ return true
+    if(this.input==''){
+      return true;
+    }
+
+    // Nếu lastChar là OP thì sau nó phải là 1 number
+    if(this.isOperator(this.getLastInputChar()) && this.isNumber(x)){
+      return true;
+    }
+    // Nếu lastChar là number thì sau nó có thể là 1 number hoặc OP
+    if(this.isNumber(this.getLastInputChar())){
+      return true;
+    }
+    else{
+      alert(this.INVALID_ERR);
+    }
+    return false;
+  }
+
+  isValidToCalculate():boolean{
+    if(this.isValidToAdd(this.input) && this.isNumber(this.getLastInputChar())){
+      return true;
+    }
+    else{
+      alert(this.INVALID_ERR)
+    }
+    return false;
+  }
+
+  isOperator(x: string): boolean {
+    let arr = ['+', '-', '*', '/'];
+    return arr.lastIndexOf(x) > -1 ? true : false;
+  }
+
+  isNumber(x: string): boolean {
+    return !isNaN(Number(x));
+  }
+
+  getLastInputChar(): string {
+    if(this.getLastInputChar() ==''){
+      this.input = '0';
+      return '0';
+    }
+    return this.input[this.input.length - 1];
+  }
+
+  remove() {
+    this.input = this.input.substring(0, this.input.length - 1);
   }
 }
 
