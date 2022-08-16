fed248040a1762f84529e30657cf73524e1abc46
zingw <vietduc.bk69@gmail.com>
2022-08-16 10:31:32 +0700

    init component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,49 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import { Injectable} from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
-  title = '';
-  a = 0;
-  b = 0;
-  arr = ['Le','Viet','Duc'];
-
-  onHandleClick(){
-    alert('Hello' + ' ' + this.title);
+  suggest =  'Put Your Address Here';
+  address = ' Hải Dương ';
+  name = 'Đức';
+  message = 'Change me by update text Box Below';
+  timeData :any;
+  SPACE = "--"
+  clickToSeeHello(){
+    alert('Hi my name is ' + this.name + " and I'm living in " + this.address )
   }
 
-  onSumClick(){
-    alert(this.a + this.b);
+  onInput(event:Event) {
+    console.log(event);
   }
 
+  onChange(event: Event) {
+    console.log(event);
+  }
+
+  constructor() {
+  }
+
+  getTime() {
+    // gán giờ hiện tại cho biến time
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDay();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let sec = date.getSeconds();
+    let milisecs =  date.getMilliseconds()
+
+    this.timeData = date;
+    console.log("Time generate Success!!!")
+  }
 
 }
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './childcomponent/child/child.component';
+import { Child2Component } from './childcomponent/child2/child2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChildComponent,
+    Child2Component,
 
   ],
   imports: [
