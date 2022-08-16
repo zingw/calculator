8961d0ae90629308c382abca85e12e93b53771ff
zingw <vietduc.bk69@gmail.com>
2022-08-16 15:34:49 +0700

    hh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  num1 = 1;
-  num2 = 2;
-  num3 = 3;
-  num4 = 4;
-  num5 = 5;
-  num6 = 6;
-  num7 = 7;
-  num8 = 8;
-  num9 = 9;
-  num0 = 0;
+  input = '';
 
-  dash = '/';
-  subtract = '-';
-  add = '+';
-  multiple = "*";
+  getAlert() {
+    alert("haha")
+  }
 
-
-
+  pressNum(s: string) {
+    this.input+= s;
+  }
 }
 
